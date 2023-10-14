27daffb3f02bfa9e1e76f2ebac16fe371fb73640
kpopino <david8.escola@gmail.com>
2023-10-14 18:27:06 -0300

    Code do password,name

--- src/main/java/br/com/davidkim/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/davidkim/todolist/filter/FilterTaskAuth.java
@@ -1,27 +1,51 @@
 package br.com.davidkim.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter{
+public class FilterTaskAuth extends OncePerRequestFilter{
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
 
-                //executar alguma acao
+                //pegar a autenticacao (usuario e senha)
+                var authorization = request.getHeader("Authorization");
                 
-                System.out.println("Chegou no filtro");
-                chain.doFilter(request, response);
+                var authEncoded = authorization.substring("Basic".length()).trim();
 
-            }
-    
+
+                byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+                var authString = new String(authDecode);
+
+
+                //["davidkim", "12345"]
+                String[] credentials = authString.split(":");
+                String username = credentials[0];
+                String password = credentials[1];
+                System.out.println("Authorization");
+                System.out.println(username);
+                System.out.println(password);
+                //validar usuario
+
+                //segue viagem
+                
+                filterChain.doFilter(request, response);
+    }
+
+
+
 }
